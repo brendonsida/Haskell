4b0aa88586ca02eb1d980d274520fabefe0410fc
cburnell <c.burnell@gmail.com>
2016-11-30 20:01:28 -0700

    changed not working example

--- examples/chuckexamples/notworking/notworkexample.hs
+++ examples/chuckexamples/notworking/notworkexample.hs
@@ -1,3 +1,7 @@
+--This doesnt work because it expects all of the parameters
+--to have the same type
 f :: a -> a -> a
-f x y = x*x + y*y
+--f :: Num a => a -> a -> a -- would work becaue now all of 
+-- the expected values are numerals
+f x y = x + y
 main =  print (f 2 2.3)
--- examples/chuckexamples/powermod/powermod.hs
+++ examples/chuckexamples/powermod/powermod.hs
@@ -1,3 +1,13 @@
-powerMod b 1 m = b `mod` m
-powerMod b e m = ( b * (powerMod b (e-1) m)) `mod` m
-main = do print (powerMod 3 3 10)
+--powerMod b 1 m = b `mod` m
+--powerMod b e m = ( b * (powerMod b (e-1) m)) `mod` m
+powermod b e m
+    | e == 1 b `mod` m
+    | otherwise = powerTail
+    where powerTail = ( b * (powerMod b (e-1) m)) `mod` m
+
+--mymax' (x:xs)   
+--        | x > maxTail = x  
+--        | otherwise = maxTail  
+--        where maxTail = mymax' xs 
+
+main = do print (powermod 3 3 10)
